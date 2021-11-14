b001d0ca474b2426ffc83a9e9cf944bf687c2f20
VDAUTREMENT <valentin.dautrement@ynov.com>
2021-11-15 00:03:36 +0100

    add first install script (not tested)

--- linux-tp3-git/scripts/backup.sh
+++ linux-tp3-git/scripts/backup.sh
@@ -1,8 +1,8 @@
- #!/bin/bash
+#!/bin/bash
 #Backup script for minecraft server project
 # Lack_off1 -- 11/11/2021
 
-file_name='$(date '+%y%m%d_%H%M%S')'
+file_name=$(date '+%y%m%d_%H%M%S')
 #archivage des fichiers
 tar -zcvf backup_${file_name}.tar.gz $2
 rsync -av --remove-source-files backup_${file_name}.tar.gz $1
